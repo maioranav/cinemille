0dbeb95a0df691e73883687f3dcaea4fdf2b1f16
Vincenzo M <vincenzomaiorana@gmail.com>
2024-06-08 11:39:53 +0200

    feature(crud): implement missing crud on services for future usage

--- src/main/java/org/vm93/cinemille/service/CinemaService.java
+++ src/main/java/org/vm93/cinemille/service/CinemaService.java
@@ -2,13 +2,16 @@ package org.vm93.cinemille.service;
 
 import java.time.LocalDate;
 import java.util.Optional;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.vm93.cinemille.model.Cinema;
+import org.vm93.cinemille.model.Film;
 import org.vm93.cinemille.repo.CinemaRepo;
 import org.vm93.cinemille.repo.ScheduleRepo;
 
+import jakarta.persistence.EntityExistsException;
 import jakarta.persistence.EntityNotFoundException;
 
 @Service
@@ -27,6 +30,30 @@ public class CinemaService {
 	public Cinema findByCinemaNo(int cinemaNo) {
 		Optional<Cinema> cinema = cinemaRepo.findByCinemaNo(cinemaNo);
 		if (!cinema.isPresent()) throw new EntityNotFoundException("Cinema not found.");
+		return cinema.get();
+	}
+	
+
+	public Cinema addCinema(Cinema cinema) {
+		if (!cinemaRepo.findByCinemaNo(cinema.getCinemaNo()).isPresent())
+			throw new EntityExistsException("CinemaNo already exists!");
+		cinemaRepo.save(cinema);
+		return cinema;
+	}
+
+	public Cinema updateCinema(UUID id, Cinema cinema) {
+		Optional<Cinema> existingCinema = cinemaRepo.findById(id);
+		if (!existingCinema.isPresent())
+			throw new EntityNotFoundException("Cinema id not found!");
+		cinemaRepo.save(cinema);
+		return cinema;
+	}
+
+	public Cinema deleteCinema(UUID id) {
+		Optional<Cinema> cinema = cinemaRepo.findById(id);
+		if (!cinema.isPresent())
+			throw new EntityNotFoundException("Cinema id not found!");
+		cinemaRepo.delete(cinema.get());
 		return cinema.get();
 	}
 
